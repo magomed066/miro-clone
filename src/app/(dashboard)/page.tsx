5d39e9d2d8f4ce94c375f8f4be3cfcc621b31337
Magomed Mamergov <mamergov@mateline.ru>
2024-02-15 23:07:55 +0300

    added organization sidebar

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const DashboardPage = () => {
-	return <div className="h-screen">Dashboard page</div>
+	return <div className="h-full">Dashboard page</div>
 }
 
 export default DashboardPage
